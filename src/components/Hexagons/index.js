e97666813cf80096853e59ef0f2a7f2dab523afb
Morgan <morganw118@gmail.com>
2021-01-30 01:54:30 -0800

    created initial skeleton for pages and app js

---
+++ src/components/Hexagons/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Hexagons() {
+    return (
+        <div></div>
+    )
+}
+
+export default Hexagons;
---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import Hexagons from "../components/Hexagons";
+
+class Home extends Component {
+
+    render() {
+        return(
+            <div>
+                <h1>Ham Is Mean to Me</h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni a eius incidunt libero ullam tenetur numquam, nulla porro.
+                    Voluptatum quasi provident tempora animi quo iste praesentium distinctio, saepe suscipit in?
+                </p>
+                <Hexagons />
+                
+            </div>
+        )
+    }
+}
+
+export default Home;
