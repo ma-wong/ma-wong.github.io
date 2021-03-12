179168c27220a08c97b577247eddfcb13ff34ccb
Morgan <morganw118@gmail.com>
2021-03-12 12:42:17 -0800

    kind of did palette change on contact page

--- src/components/ContactCard/index.js
+++ src/components/ContactCard/index.js
@@ -7,95 +7,93 @@ import "./style.css";
 
 function ContactCard() {
     return (
-        <div className="container">
-            <div className="card contact-card">
-                {/* <div className="card-body"> */}
-                    <div className="row">
+        <div className="card contact-card">
+            
+            <div className="row">
 
-                        {/* Contact Form */}
-                        <div className="col-md-8 contact-input">
-                            <h2 id="contact-input-header">CONTACT ME</h2>
-                            
-                            <form>
+                {/* Contact Form */}
+                <div className="col-md-8 contact-input">
+                    <h2 id="contact-input-header">CONTACT ME</h2>
+                    
+                    <form>
 
-                                {/* name input */}
-                                <div className="row">
-                                    <div className="col-md-6 mb-3">
-                                        <label for="validationDefault01" className="form-label">First name</label>
-                                        <input type="text" className="form-control" id="validationDefault01" required />
-                                    </div>
+                        {/* name input */}
+                        <div className="row">
+                            <div className="col-md-6 mb-3">
+                                <label for="validationDefault01" className="form-label contact-form-label">First name</label>
+                                <input type="text" className="form-control contact-form-input" id="validationDefault01" required />
+                            </div>
 
-                                    <div className="col-md-6 mb-3">
-                                        <label for="validationDefault02" className="form-label">Last name</label>
-                                        <input type="text" className="form-control" id="validationDefault02" required />
-                                    </div>
-                                </div>
-                                {/* email input */}
-                                <div className="row">
-                                    <div className="col-md-12 mb-3">
-                                        <label for="validationDefault03" className="form-label">Email address</label>
-                                        <input type="email" className="form-control" id="validationDefault03" placeholder="name@example.com" required />
-                                    </div>
-                                </div>
-
-                                {/* comment input */}
-                                <div className="row">
-                                    <div className="col-md-12 mb-3">
-                                        <label for="validationDefault05" className="form-label">Comments</label>
-                                        <textarea type="text" className="form-control" id="validationDefault05" rows="8" required />
-                                    </div>
-                                </div>
-                                
-                                {/* submit btn */}
-                                <div className="d-grid gap-2 col-6 mx-auto">
-                                    <button id="submit-btn" className="btn btn-success" type="submit">Submit</button>
-                                </div>
-                                
-                            </form>
-
+                            <div className="col-md-6 mb-3">
+                                <label for="validationDefault02" className="form-label contact-form-label">Last name</label>
+                                <input type="text" className="form-control contact-form-input" id="validationDefault02" required />
+                            </div>
+                        </div>
+                        {/* email input */}
+                        <div className="row">
+                            <div className="col-md-12 mb-3">
+                                <label for="validationDefault03" className="form-label contact-form-label">Email address</label>
+                                <input type="email" className="form-control contact-form-input" id="validationDefault03" placeholder="name@example.com" required />
+                            </div>
                         </div>
 
-
-
-                        {/* Contact Info */}
-                        <div id="contact-info-div" className="col-md-4">
+                        {/* comment input */}
+                        <div className="row">
+                            <div className="col-md-12 mb-3">
+                                <label for="validationDefault05" className="form-label contact-form-label">Comments</label>
+                                <textarea type="text" className="form-control contact-form-input" id="validationDefault05" rows="8" required />
+                            </div>
+                        </div>
                         
-                            <div className="row contact-info">
-                                <h5>EMAIL</h5>
-                                <p className="contact-p">mw.web.develop@gmail.com</p>
-                            </div>
-
-                            <div className="row contact-info">
-                                <h5>TELEPHONE</h5>
-                                <p className="contact-p">(949) 337-8617</p>
-                            </div>
-
-                            <div className="row contact-info">
-                                <h5>LOCATION</h5>
-                                <p className="contact-p">Irvine, CA</p>
-                            </div>
-                            
-                            <div className="icon-container contact-info">
-                                <a href="https://github.com/ma-wong" target="_blank" rel="noopener noreferrer">
-                                    <img id="gh-icon" className="contact-icons" src={githubIcon}></img>
-                                </a>
-                                <a href="https://linkedin.com/in/morgan-wong-95b305207" target="_blank" rel="noopener noreferrer">
-                                    <img className="contact-icons" src={linkedInIcon}></img>
-                                </a>
-                            </div>
-
-                            <div>
-                                <a href={resumeDownload} download="Resume">
-                                    <button id="download-btn" type="button" className="btn btn-outline-light" >DOWNLOAD RESUME <i className="fa fa-file-pdf-o"></i></button>
-                                </a>
-                            </div>
-
+                        {/* submit btn */}
+                        <div className="d-grid gap-2 col-6 mx-auto">
+                            <button id="submit-btn" className="btn btn-success" type="submit">Submit</button>
                         </div>
+                        
+                    </form>
+
+                </div>
 
 
+
+                {/* Contact Info */}
+                <div id="contact-info-div" className="col-md-4">
+                
+                    <div className="row contact-info">
+                        <h5>EMAIL</h5>
+                        <p className="contact-p">mw.web.develop@gmail.com</p>
                     </div>
-                {/* </div> */}
+
+                    <div className="row contact-info">
+                        <h5>TELEPHONE</h5>
+                        <p className="contact-p">(949) 337-8617</p>
+                    </div>
+
+                    <div className="row contact-info">
+                        <h5>LOCATION</h5>
+                        <p className="contact-p">Irvine, CA</p>
+                    </div>
+                    
+                    <div className="icon-container contact-info">
+                        <a href="https://github.com/ma-wong" target="_blank" rel="noopener noreferrer">
+                            <img id="gh-icon" className="contact-icons" src={githubIcon}></img>
+                        </a>
+                        <a href="https://linkedin.com/in/morgan-wong-95b305207" target="_blank" rel="noopener noreferrer">
+                            <img className="contact-icons" src={linkedInIcon}></img>
+                        </a>
+                    </div>
+
+                    <div>
+                        <a href={resumeDownload} download="Resume">
+                            <button id="download-btn" type="button" className="btn btn-outline-light" >DOWNLOAD RESUME <i className="fa fa-file-pdf-o"></i></button>
+                        </a>
+                    </div>
+
+                </div>
+
+
             </div>
+            
         </div>
     )
 }
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,13 +1,23 @@
 import React, { Component } from "react";
 import ContactCard from "../components/ContactCard";
-import Jumbotron from "../components/Jumbotron";
+// import Jumbotron from "../components/Jumbotron";
 
 class Contact extends Component {
     
     render() {
         return (
-            <div>
-                <Jumbotron />
+            <div className="container">
+
+                <div className="row about-header-row">
+                    <div className="col-md-3"></div>
+                    <div className="col-md-6">
+                        <hr />
+                        <h1 id="about-header">GET IN TOUCH</h1>
+                        <hr />
+                    </div>
+                    <div className="col-md-3"></div>
+                </div>
+                
                 <ContactCard />
             </div>
         )
