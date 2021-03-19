efdf0110f064cb8e9c240c170330ff443d91331b
Morgan <morganw118@gmail.com>
2021-03-18 17:45:03 -0700

    played with restructure of project page

--- src/components/ProjectCard/index.js
+++ src/components/ProjectCard/index.js
@@ -11,6 +11,9 @@ import Snippets1 from "./img/Snippets/home.png";
 import Snippets2 from "./img/Snippets/browse.png";
 import Snippets3 from "./img/Snippets/details.png";
 
+import githubIcon from "./icons/github-project.png";
+import deployedIcon from "./icons/deployed-project.png";
+
 
 import "./style.css"
 
@@ -19,7 +22,7 @@ function ProjectCard() {
         <div>
 
             {/* Card 1 */}
-            <div className="card mb-3 project-card">
+            {/* <div className="card mb-3 project-card">
                 <div className="row g-0">
                     
                     <div id="carouselExampleIndicators-1" className="carousel slide col-md-7" data-bs-ride="carousel">
@@ -69,6 +72,88 @@ function ProjectCard() {
                             </div>
                                              
                             <p id="portfolio-footer" className="card-text"><small className="text-muted">Last updated: 1/15/2021</small></p>
+                        </div>
+                    </div>
+                </div>
+            </div> */}
+
+            <div className="card mb-3 project-card">
+                <div className="row g-0">
+                    
+                    <div id="carouselExampleIndicators-1" className="carousel slide col-md-7" data-bs-ride="carousel">
+                        <ol className="carousel-indicators">
+                            <li data-bs-target="#carouselExampleIndicators-1" data-bs-slide-to="0" className="active"></li>
+                            <li data-bs-target="#carouselExampleIndicators-1" data-bs-slide-to="1"></li>
+                            <li data-bs-target="#carouselExampleIndicators-1" data-bs-slide-to="2"></li>
+                        </ol>
+                        <div className="carousel-inner">
+                            <div className="carousel-item active">
+                            <img src={Snippets1} className="d-block w-100 project-img" alt="..."/>
+                            </div>
+                            <div className="carousel-item">
+                            <img src={Snippets2} className="d-block w-100 project-img" alt="..."/>
+                            </div>
+                            <div className="carousel-item">
+                            <img src={Snippets3} className="d-block w-100 project-img" alt="..."/>
+                            </div>
+                        </div>
+                        <a className="carousel-control-prev" href="#carouselExampleIndicators-1" role="button" data-bs-slide="prev">
+                            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span className="visually-hidden">Previous</span>
+                        </a>
+                        <a className="carousel-control-next" href="#carouselExampleIndicators-1" role="button" data-bs-slide="next">
+                            <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span className="visually-hidden">Next</span>
+                        </a>
+                    </div>
+                    
+                    <div className="col-md-5">
+                        <div className="card-body project-card-body1">
+
+                            <h5 className="card-title portfolio-card-title">Snippets</h5>
+                            <p className="card-text project-description1">
+                                Snippets is an online application that allowd users to store snippets of code in a single organized library
+                                so that they can re-use boiler plate sections of their code in future projects. It also allows the user to search
+                                for keywords to return boiler plate code snippets that others have submitted publicaly.
+                            </p>
+
+                            <div className="project-tools">
+                                <div className="row">
+                                    <p className="col-md-3 tools-row1">React</p>
+                                    <p className="col-md-3 tools-row1">Node.js</p>
+                                    <p className="col-md-3 tools-row1">Express</p>
+                                    <p className="col-md-3 tools-row1">Highlight.js</p>
+                                </div>
+                                <div className="row">
+                                    <p className="col-md-3">HTML/CSS</p>
+                                    <p className="col-md-3">MySQL</p>
+                                    <p className="col-md-3">Cloudinary</p>
+                                    <p className="col-md-3">Passport</p>
+                                </div>
+                            </div>
+
+                            <div className="row">
+                                <div className="col-md-8"></div>
+                                <a className="col-md-2" href="https://github.com/ma-wong" target="_blank" rel="noopener noreferrer">
+                                    <img  className="project-icons" src={githubIcon}></img>
+                                </a>
+                                <a className="col-md-2" href="https://linkedin.com/in/morgan-wong-95b305207" target="_blank" rel="noopener noreferrer">
+                                    <img className="project-icons" src={deployedIcon}></img>
+                                </a>
+                                {/* <div className="col-md-8"></div> */}
+
+                            </div>
+
+                            {/* <div className="row">
+                                <a href="https://github.com/ma-wong/project-3" target="_blank" className="col-md-6">
+                                    <button class="shrink-border project-btns">Github Repo</button>
+                                </a>
+                                <a href="#" className="col-md-6" target="_blank">
+                                    <button class="shrink-border project-btns">Deployed App</button>
+                                </a>                               
+                            </div>
+                                             
+                            <p id="portfolio-footer" className="card-text"><small className="text-muted">Last updated: 1/15/2021</small></p> */}
                         </div>
                     </div>
                 </div>
