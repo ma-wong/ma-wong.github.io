604513b5f1020c2dbe984fb0456f03e8cfb21820
Morgan <morganw118@gmail.com>
2021-03-22 14:03:11 -0700

    restructured hexagon layout

--- src/components/Hexagons/test.js
+++ src/components/Hexagons/test.js
@@ -38,8 +38,109 @@ function Hexagons() {
 
                 <li className="hex">
                     <div className="hexIn">
+                    <a className="hexLink" href="#">
+                        <div className='img' style={{ backgroundImage: `url(${mySqlLogo})` }}></div>
+                        <h1 id="demo1">MySql</h1>
+                        {/* <p id="demo2">Open-source relational database management system.</p> */}
+                    </a>
+                    </div>
+                </li>
+
+                <li className="hex">
+                    <div className="hexIn">
+                    <a className="hexLink" href="#">
+                        <div className='img' style={{ backgroundImage: `url(${nodeLogo})` }}></div>
+                        <h1 id="demo1">Node JS</h1>
+                        {/* <p id="demo2">Back-end runtime environment that executes JavaScript code outside a web browser.</p> */}
+                    </a>
+                    </div>
+                </li>
+
+                <li className="hex">
+                    <div className="hexIn">
                     <a id="grayscale" className="hexLink" href="#">
                         <div className='img' style={{ backgroundImage: `url(${carousel})` }}></div>
+                        <h1 id="demo1">MySql</h1>
+                        {/* <p id="demo2">Open-source relational database management system.</p> */}
+                    </a>
+                    </div>
+                </li>
+
+                <li className="hex">
+                    <div className="hexIn">
+                    <a className="hexLink" href="#">
+                    <div className='img' style={{ backgroundImage: `url(${reactLogo})` }}></div>
+                        <h1 id="demo1">React</h1>
+                        {/* <p id="demo2">Front end, JavaScript library for building user interfaces or UI components.</p> */}
+                    </a>
+                    </div>
+                </li>
+
+                <li className="hex">
+                    <div className="hexIn">
+                    <a className="hexLink" href="#">
+                            <div className='img' style={{ backgroundImage: `url(${jsLogo})` }}></div>
+                        <h1 id="demo1">Javascript</h1>
+                        {/* <p id="demo2">High-level programming language that conforms to the ECMAScript specification.</p> */}
+                    </a>
+                    </div>
+                </li>
+
+                <li className="hex">
+                    <div className="hexIn">
+                    <a id="grayscale" className="hexLink" href="#">
+                        <div className='img' style={{ backgroundImage: `url(${girls})` }}></div>
+                        <h1 id="demo1">MySql</h1>
+                        {/* <p id="demo2">Open-source relational database management system.</p> */}
+                    </a>
+                    </div>
+                </li>
+
+                <li className="hex">
+                    <div className="hexIn">
+                    <a className="hexLink" href="#">
+                        <div className='img' style={{ backgroundImage: `url(${htmlLogo})` }}></div>
+                        <h1 id="demo1">HTML</h1>
+                        {/* <p id="demo2">Standard language for documents designed to be displayed in a web browser.</p> */}
+                    </a>
+                    </div>
+                </li>
+
+                <li className="hex">
+                    <div className="hexIn">
+                    <a id="grayscale" className="hexLink" href="#">
+                        <div className='img' style={{ backgroundImage: `url(${stage})` }}></div>
+                        <h1 id="demo1">MySql</h1>
+                        {/* <p id="demo2">Open-source relational database management system.</p> */}
+                    </a>
+                    </div>
+                </li>
+
+                <li className="hex">
+                    <div className="hexIn">
+                    <a id="grayscale" className="hexLink" href="#">
+                        <div className='img' style={{ backgroundImage: `url(${sunset})` }}></div>
+                        <h1 id="demo1">MySql</h1>
+                        {/* <p id="demo2">Open-source relational database management system.</p> */}
+                    </a>
+                    </div>
+                </li>
+
+                
+                <li className="hex">
+                    <div className="hexIn">
+                    <a className="hexLink" href="#">
+                        <div className='img' style={{ backgroundImage: `url(${cssLogo})` }} ></div>
+                        <h1 id="demo1">CSS</h1>
+                        {/* <p id="demo2">Style sheet language used for describing the presentation of a document</p> */}
+                    </a>
+                    </div>
+                </li>
+
+                <li className="hex">
+                    <div className="hexIn">
+                    <a id="grayscale" className="hexLink" href="#">
+                        <div className='img' style={{ backgroundImage: `url(${gator})` }}></div>
                         <h1 id="demo1">MySql</h1>
                         {/* <p id="demo2">Open-source relational database management system.</p> */}
                     </a>
@@ -59,108 +160,14 @@ function Hexagons() {
                 <li className="hex">
                     <div className="hexIn">
                     <a className="hexLink" href="#">
-                        <div className='img' style={{ backgroundImage: `url(${gator})` }}></div>
-                        <h1 id="demo1">MySql</h1>
-                        {/* <p id="demo2">Open-source relational database management system.</p> */}
-                    </a>
-                    </div>
-                </li>
-
-                <li className="hex">
-                    <div className="hexIn">
-                    <a className="hexLink" href="#">
-                        <div className='img' style={{ backgroundImage: `url(${girls})` }}></div>
-                        <h1 id="demo1">MySql</h1>
-                        {/* <p id="demo2">Open-source relational database management system.</p> */}
-                    </a>
-                    </div>
-                </li>
-
-                <li className="hex">
-                    <div className="hexIn">
-                    <a className="hexLink" href="#">
-                        <div className='img' style={{ backgroundImage: `url(${stage})` }}></div>
-                        <h1 id="demo1">MySql</h1>
-                        {/* <p id="demo2">Open-source relational database management system.</p> */}
-                    </a>
-                    </div>
-                </li>
-
-                <li className="hex">
-                    <div className="hexIn">
-                    <a className="hexLink" href="#">
-                        <div className='img' style={{ backgroundImage: `url(${sunset})` }}></div>
-                        <h1 id="demo1">MySql</h1>
-                        {/* <p id="demo2">Open-source relational database management system.</p> */}
-                    </a>
-                    </div>
-                </li>
-
-                {/* Skills */}
-                <li className="hex">
-                    <div className="hexIn">
-                    <a className="hexLink" id="grayscale" href="#">
-                        <div className='img' style={{ backgroundImage: `url(${mySqlLogo})` }}></div>
-                        <h1 id="demo1">MySql</h1>
-                        {/* <p id="demo2">Open-source relational database management system.</p> */}
-                    </a>
-                    </div>
-                </li>
-                    
-                <li className="hex">
-                    <div className="hexIn">
-                    <a className="hexLink" href="#">
-                            <div className='img' style={{ backgroundImage: `url(${jsLogo})` }}></div>
-                        <h1 id="demo1">Javascript</h1>
-                        {/* <p id="demo2">High-level programming language that conforms to the ECMAScript specification.</p> */}
-                    </a>
-                    </div>
-                </li>
-                <li className="hex">
-                    <div className="hexIn">
-                    <a className="hexLink" href="#">
-                        <div className='img' style={{ backgroundImage: `url(${nodeLogo})` }}></div>
-                        <h1 id="demo1">Node JS</h1>
-                        {/* <p id="demo2">Back-end runtime environment that executes JavaScript code outside a web browser.</p> */}
-                    </a>
-                    </div>
-                </li>
-                <li className="hex">
-                    <div className="hexIn">
-                    <a className="hexLink" href="#">
-                        <div className='img' style={{ backgroundImage: `url(${cssLogo})` }} ></div>
-                        <h1 id="demo1">CSS</h1>
-                        {/* <p id="demo2">Style sheet language used for describing the presentation of a document</p> */}
-                    </a>
-                    </div>
-                </li>
-                <li className="hex">
-                    <div className="hexIn">
-                    <a className="hexLink" href="#">
                         <div className='img' style={{ backgroundImage: `url(${mongoLogo})` }}></div>
                         <h1 id="demo1">Mongo DB</h1>
                         {/* <p id="demo2">A cross-platform document-oriented database program</p> */}
                     </a>
                     </div>
                 </li>
-                <li className="hex">
-                    <div className="hexIn">
-                    <a className="hexLink" href="#">
-                    <div className='img' style={{ backgroundImage: `url(${reactLogo})` }}></div>
-                        <h1 id="demo1">React</h1>
-                        {/* <p id="demo2">Front end, JavaScript library for building user interfaces or UI components.</p> */}
-                    </a>
-                    </div>
-                </li>
-                <li className="hex">
-                    <div className="hexIn">
-                    <a className="hexLink" href="#">
-                        <div className='img' style={{ backgroundImage: `url(${htmlLogo})` }}></div>
-                        <h1 id="demo1">HTML</h1>
-                        {/* <p id="demo2">Standard language for documents designed to be displayed in a web browser.</p> */}
-                    </a>
-                    </div>
-                </li>
+                
+                
                 
 
 
