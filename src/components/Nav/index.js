f14ea18244a3e20d0b0e7c699816e7996e013fb3
Morgan <morganw118@gmail.com>
2021-03-30 17:44:15 -0700

    testing offcanvas nav - kind of working

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -5,6 +5,8 @@ import "./style.css"
 
 function Nav() {
     return (
+
+        // ------- OLD CENTERED NAV -----
         // <div id="nav-container">
         //     <nav className="navbar navbar-expand-lg navbar-dark">
         //         <div className="container-fluid">
@@ -48,19 +50,71 @@ function Nav() {
         //     </nav>
         // </div>
 
+        // ---- LEFT ALIGNED NAV ------
+        // <div id="nav-container" className="container">
+        //     <nav className="navbar navbar-expand-lg navbar-dark">
+        //         <div className="container-fluid">
+        //             <Link to= {"/"}>
+        //                 <a className="navbar-brand" href="#">
+        //                     <img id="nav-logo" src={logo} alt="" width="30" height="24"/>
+        //                 </a>
+        //             </Link>
+
+        //             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        //                 <span className="navbar-toggler-icon"></span>
+        //             </button>
+
+        //             <div className="collapse navbar-collapse" id="navbarNav">
+        //                 <ul className="navbar-nav d-flex justify-content-end">
+        //                     <Link to={"/about"}>
+        //                         <li className="nav-link">ABOUT</li>
+        //                     </Link>
+        //                     <Link to={"/portfolio"}>
+        //                         <li className="nav-link">PORTFOLIO</li>
+        //                     </Link>
+        //                     <Link to={"/contact"}>
+        //                         <li className="nav-link">CONTACT</li>
+        //                     </Link>
+        //                 </ul>
+        //             </div>
+        //         </div>
+        //     </nav>
+        // </div>
+        
+        // ---- TESTING------
         <div id="nav-container" className="container">
             <nav className="navbar navbar-expand-lg navbar-dark">
                 <div className="container-fluid">
                     <Link to= {"/"}>
-                        <a className="navbar-brand" href="#">
-                            <img id="nav-logo" src={logo} alt="" width="30" height="24"/>
-                        </a>
+                        <img id="nav-logo" src={logo} alt="" width="30" height="24"/>
                     </Link>
 
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    {/* Nav Buttonn */}
+                    <button className="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasWithBothOptions" aria-controls="offcanvasWithBothOptions">
                         <span className="navbar-toggler-icon"></span>
                     </button>
 
+                    {/* Off Canvas nav content */}
+                    <div className="offcanvas offcanvas-end" data-bs-scroll="true" tabindex="-1" id="offcanvasWithBothOptions" aria-labelledby="offcanvasWithBothOptionsLabel">
+                        <div className="offcanvas-header">
+                            <button type="button" className="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+                        </div>
+                        <div className="offcanvas-body">
+                            <ul>
+                                <Link to={"/about"}>
+                                    <li className="nav-link">ABOUT</li>
+                                </Link>
+                                <Link to={"/portfolio"}>
+                                    <li className="nav-link">PORTFOLIO</li>
+                                </Link>
+                                <Link to={"/contact"}>
+                                    <li className="nav-link">CONTACT</li>
+                                </Link>
+                            </ul>
+                        </div>
+                    </div>
+
+                    {/* Normal left aligned Nav */}
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav d-flex justify-content-end">
                             <Link to={"/about"}>
@@ -74,9 +128,13 @@ function Nav() {
                             </Link>
                         </ul>
                     </div>
+
+
                 </div>
             </nav>
         </div>
+
+        
     )
 }
 
