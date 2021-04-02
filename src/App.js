43dfb15f48fac53bc0017d784645bc0e0cd63e83
Morgan <morganw118@gmail.com>
2021-04-02 15:52:24 -0700

    testing out splash pages- finished first animation

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
 import About from "./pages/About";
 import Portfolio from "./pages/Portfolio";
 import Contact from "./pages/Contact";
 import Nav from "./components/Nav";
+import Splash from "./pages/Splash";
 // import Footer from"./components/Footer";
 import "./style.css";
 
@@ -14,7 +15,7 @@ function App() {
       <div>
         <Nav />
         <Switch>
-          <Route exact path="/" component={Home} />
+          <Route exact path="/" component={Splash} />
           <Route exact path="/about" component={About} />
           <Route exact path="/portfolio" component={Portfolio} />
           <Route exact path="/contact" component={Contact} />
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,13 +4,16 @@ import "./style.css";
 function Header() {
     
     return(
-        <header className="home-header">
-            <p id="home-title">Hello, my name is</p>
-            <p id="home-name">MORGAN WONG</p>
-            <p id="home-description">
-                Full-stack web developer / Designer
-            </p>
-        </header>
+        <div className="container">
+            <header className="home-header">
+                <p id="home-title">Hello, my name is</p>
+                <p id="home-name">MORGAN WONG</p>
+                <p id="home-description">
+                    Full-stack web developer / Designer
+                </p>
+            </header>
+        </div>
+        
     )
 }
 
