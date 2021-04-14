faa2ccb8c01b73288622136a728608af6e690e00
Morgan <morganw118@gmail.com>
2021-04-14 16:36:57 -0700

    created basic html skeleton for project tiles

--- src/components/ProjectTiles/index.js
+++ src/components/ProjectTiles/index.js
@@ -4,62 +4,103 @@ import "./style.css";
 function ProjectTiles() {
     
     return(
-        
-        <ul className="tile-grid">
-            <div className="row">
-                <li className="col-md-3 project-tile">
-                    <a href="#">
-                    <div className="tile-primary-content">
-                        <img src="http://albumcoverart.files.wordpress.com/2010/04/fleet-foxes-lp.jpg" alt=""/>
-                    </div>
-                    <div className="tile-secondary-content">
-                        <h2>Music</h2>
-                        <p>Fleet Foxes</p>
-                    </div>
-                    </a>
-                </li>
+ 
+        <div>
 
-                <li className="col-md-3 project-tile">
-                    <a href="#">
-                    <div className="tile-primary-content">
-                        <img src="http://albumcoverart.files.wordpress.com/2010/04/fleet-foxes-lp.jpg" alt=""/>
+            <div className="row wrap">
+                <div className="tile">
+                    <img src='https://images.unsplash.com/photo-1464054313797-e27fb58e90a9?dpr=1&auto=format&crop=entropy&fit=crop&w=1500&h=996&q=80' />
+                    <div className="text">
+                    <h1>Lorem ipsum.</h1>
+                    <h2 className="animate-text">More lorem ipsum bacon ipsum.</h2>
+                    <p className="animate-text">Bacon ipsum dolor amet pork belly tri-tip turducken, pancetta bresaola pork chicken meatloaf. Flank sirloin strip steak prosciutto kevin turducken. </p>
+                    <div className="dots">
+                        <span></span>
+                        <span></span>
+                        <span></span>
                     </div>
-                    <div className="tile-secondary-content">
-                        <h2>Music</h2>
-                        <p>Fleet Foxes</p>
                     </div>
-                    </a>
-                </li>
+                </div>
 
-                <li className="col-md-3 project-tile">
-                    <a href="#">
-                    <div className="tile-primary-content">
-                        <img src="http://albumcoverart.files.wordpress.com/2010/04/fleet-foxes-lp.jpg" alt=""/>
+                <div className="tile">
+                    <img src='https://images.unsplash.com/photo-1458668383970-8ddd3927deed?dpr=1&auto=format&crop=entropy&fit=crop&w=1500&h=1004&q=80' />
+                    <div className="text">
+                    <h1>Lorem ipsum.</h1>
+                    <h2 className="animate-text">More lorem ipsum bacon ipsum.</h2>
+                    <p className="animate-text">Bacon ipsum dolor amet pork belly tri-tip turducken, pancetta bresaola pork chicken meatloaf. Flank sirloin strip steak prosciutto kevin turducken. </p>
+                    <div className="dots">
+                        <span></span>
+                        <span></span>
+                        <span></span>
                     </div>
-                    <div className="tile-secondary-content">
-                        <h2>Music</h2>
-                        <p>Fleet Foxes</p>
                     </div>
-                    </a>
-                </li>
+                </div>
 
-                <li className="col-md-3 project-tile">
-                    <a href="#">
-                    <div className="tile-primary-content">
-                        <img src="http://albumcoverart.files.wordpress.com/2010/04/fleet-foxes-lp.jpg" alt=""/>
+                <div className="tile">
+                    <img src='https://images.unsplash.com/photo-1422393462206-207b0fbd8d6b?dpr=1&auto=format&crop=entropy&fit=crop&w=1500&h=1000&q=80' />
+                    <div className="text">
+                    <h1>Lorem ipsum.</h1>
+                    <h2 className="animate-text">More lorem ipsum bacon ipsum.</h2>
+                    <p className="animate-text">Bacon ipsum dolor amet pork belly tri-tip turducken, pancetta bresaola pork chicken meatloaf. Flank sirloin strip steak prosciutto kevin turducken. </p>
+                    <div className="dots">
+                        <span></span>
+                        <span></span>
+                        <span></span>
                     </div>
-                    <div className="tile-secondary-content">
-                        <h2>Music</h2>
-                        <p>Fleet Foxes</p>
                     </div>
-                    </a>
-                </li>
+                </div>
 
             </div>
 
-            
+            <div className="row wrap">
+
+                <div className="tile">
+                    <img src='https://images.unsplash.com/photo-1422393462206-207b0fbd8d6b?dpr=1&auto=format&crop=entropy&fit=crop&w=1500&h=1000&q=80' />
+                    <div className="text">
+                    <h1>Lorem ipsum.</h1>
+                    <h2 className="animate-text">More lorem ipsum bacon ipsum.</h2>
+                    <p className="animate-text">Bacon ipsum dolor amet pork belly tri-tip turducken, pancetta bresaola pork chicken meatloaf. Flank sirloin strip steak prosciutto kevin turducken. </p>
+                    <div className="dots">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                    </div>
+                </div>
+
+                <div className="tile">
+                    <img src='https://images.unsplash.com/photo-1422393462206-207b0fbd8d6b?dpr=1&auto=format&crop=entropy&fit=crop&w=1500&h=1000&q=80' />
+                    <div className="text">
+                    <h1>Lorem ipsum.</h1>
+                    <h2 className="animate-text">More lorem ipsum bacon ipsum.</h2>
+                    <p className="animate-text">Bacon ipsum dolor amet pork belly tri-tip turducken, pancetta bresaola pork chicken meatloaf. Flank sirloin strip steak prosciutto kevin turducken. </p>
+                    <div className="dots">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                    </div>
+                </div>
+
+                <div className="tile">
+                    <img src='https://images.unsplash.com/photo-1422393462206-207b0fbd8d6b?dpr=1&auto=format&crop=entropy&fit=crop&w=1500&h=1000&q=80' />
+                    <div className="text">
+                    <h1>Lorem ipsum.</h1>
+                    <h2 className="animate-text">More lorem ipsum bacon ipsum.</h2>
+                    <p className="animate-text">Bacon ipsum dolor amet pork belly tri-tip turducken, pancetta bresaola pork chicken meatloaf. Flank sirloin strip steak prosciutto kevin turducken. </p>
+                    <div className="dots">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                    </div>
+                </div>
+
+            </div>
+
+
+        </div>
         
-        </ul>
     )
 }
 
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -2,7 +2,8 @@ import React, { Component } from "react";
 import ProjectCard from "../components/ProjectCard";
 import Footer from "../components/Footer";
 import PageHeader from "../components/PageHeader";
-// import ProjectTiles from "../components/ProjectTiles";
+import ProjectTiles from "../components/ProjectTiles";
+import "../pageStyles/Portfolio.css";
 
 class Portfolio extends Component {
 
@@ -24,7 +25,8 @@ class Portfolio extends Component {
                 
                 <ProjectCard />
 
-                {/* <ProjectTiles /> */}
+                <p id="tiles-header">Other Noteworthy Projects</p>
+                <ProjectTiles />
 
                 <Footer />
             </div>
