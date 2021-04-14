bf8c9590b9859cab55b0be37db245ba00024e604
Morgan <morganw118@gmail.com>
2021-04-14 15:30:54 -0700

    contact form resets after message is submitted

--- src/components/ContactCard/index.js
+++ src/components/ContactCard/index.js
@@ -23,11 +23,8 @@ function ContactCard() {
         }, (error) => {
             console.log(error.text);
         });
-    }
-
-    // function clearFields() {
-
-    // }
+        e.target.reset();
+    };
 
     return (
 
@@ -149,10 +146,29 @@ function ContactCard() {
 
                     </div>
 
+                    
+
 
                 </div>
                 
             </div>
+
+            {/* <button type="button" class="btn btn-primary" id="liveToastBtn">Show live toast</button>
+
+            <div class="position-fixed bottom-0" style={{"z-index": 5}}>
+                <div id="liveToast" class="toast" role="alert" aria-live="assertive" aria-atomic="true">
+                    <div class="toast-header">
+                    <img src="..." class="rounded me-2" alt="..."/>
+                    <strong class="me-auto">Bootstrap</strong>
+                    <small>11 mins ago</small>
+                    <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+                    </div>
+                    <div class="toast-body">
+                    Hello, world! This is a toast message.
+                    </div>
+                </div>
+            </div> */}
+
         </div>
     )
 }
