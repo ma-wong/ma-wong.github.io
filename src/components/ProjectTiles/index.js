f149334b0f0d36c6cacb39876d842994ed3684bc
Morgan <morganw118@gmail.com>
2021-04-15 16:26:47 -0700

    added some of the employee tracker to the tiles

--- src/components/ProjectTiles/index.js
+++ src/components/ProjectTiles/index.js
@@ -17,11 +17,11 @@ function ProjectTiles() {
                         <h2 className="animate-text">Archie's Ice Cream</h2>
                         <p className="animate-text">
                             Complete overhaul and redesign of Archie's Ice Cream website using Squarespace.
-                            Custom graphics/logos were made with photoshop and Procreate.
+                            Custom graphics/logos were made with photoshop and Procreate
                         </p>
                         <div className="animate-text tile-icon-div">
                             <a href="" className="tile-link"><FontAwesomeIcon className="tile-icon" icon={['fab', 'github']}/></a>
-                            <a href="" className="tile-link"><FontAwesomeIcon className="tile-icon" icon="external-link-alt"/></a>
+                            <a href="https://www.archiesicecream.com" className="tile-link"><FontAwesomeIcon className="tile-icon" icon="external-link-alt"/></a>
                         </div>
                         <div className="dots">
                             <span></span>
@@ -34,11 +34,13 @@ function ProjectTiles() {
                 <div className="tile">
                     <img src='https://images.unsplash.com/photo-1458668383970-8ddd3927deed?dpr=1&auto=format&crop=entropy&fit=crop&w=1500&h=1004&q=80' />
                     <div className="text">
-                        {/* <h1>Lorem ipsum.</h1> */}
-                        <h2 className="animate-text">More lorem ipsum bacon ipsum.</h2>
-                        <p className="animate-text">Bacon ipsum dolor amet pork belly tri-tip turducken, pancetta bresaola pork chicken meatloaf. Flank sirloin strip steak prosciutto kevin turducken. </p>
+                        <h2 className="animate-text">Employee Tracker</h2>
+                        <p className="animate-text">
+                            A terminal based app that allows business owners to view/manage employees, department, and roles withinn a designated employee database.
+                            Built with Javascript, jQuery, mySQL, and Node.js
+                        </p>
                         <div className="animate-text tile-icon-div">
-                            <a href="" className="tile-link"><FontAwesomeIcon className="tile-icon" icon={['fab', 'github']}/></a>
+                            <a href="https://github.com/ma-wong/employee-tracker" className="tile-link"><FontAwesomeIcon className="tile-icon" icon={['fab', 'github']}/></a>
                             <a href="" className="tile-link"><FontAwesomeIcon className="tile-icon" icon="external-link-alt"/></a>
                         </div>
                         <div className="dots">
