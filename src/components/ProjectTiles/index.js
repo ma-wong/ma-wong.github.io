147b3d99ee5c1073f9b85ecc0deb80dbc6fb4336
Morgan <morganw118@gmail.com>
2021-04-14 19:08:46 -0700

    added archies to the tiles

--- src/components/ProjectTiles/index.js
+++ src/components/ProjectTiles/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./style.css";
 
 function ProjectTiles() {
@@ -8,24 +9,26 @@ function ProjectTiles() {
         <div>
 
             <div className="row wrap">
-                <div className="tile">
+                <a className="tile" href="https://www.archiesicecream.com">
+                {/* <div className="tile"> */}
                     <img src='https://images.unsplash.com/photo-1464054313797-e27fb58e90a9?dpr=1&auto=format&crop=entropy&fit=crop&w=1500&h=996&q=80' />
                     <div className="text">
-                    <h1>Lorem ipsum.</h1>
-                    <h2 className="animate-text">More lorem ipsum bacon ipsum.</h2>
-                    <p className="animate-text">Bacon ipsum dolor amet pork belly tri-tip turducken, pancetta bresaola pork chicken meatloaf. Flank sirloin strip steak prosciutto kevin turducken. </p>
+                    {/* <h1>Lorem ipsum.</h1> */}
+                    <h2 className="animate-text">Archie's Ice Cream</h2>
+                    <p className="animate-text">Responsible for the complete redesign and restructure of Archie's Ice Cream website using squarespace and photoshop for graphics.</p>
                     <div className="dots">
                         <span></span>
                         <span></span>
                         <span></span>
                     </div>
                     </div>
-                </div>
+                {/* </div> */}
+                </a>
 
                 <div className="tile">
                     <img src='https://images.unsplash.com/photo-1458668383970-8ddd3927deed?dpr=1&auto=format&crop=entropy&fit=crop&w=1500&h=1004&q=80' />
                     <div className="text">
-                    <h1>Lorem ipsum.</h1>
+                    {/* <h1>Lorem ipsum.</h1> */}
                     <h2 className="animate-text">More lorem ipsum bacon ipsum.</h2>
                     <p className="animate-text">Bacon ipsum dolor amet pork belly tri-tip turducken, pancetta bresaola pork chicken meatloaf. Flank sirloin strip steak prosciutto kevin turducken. </p>
                     <div className="dots">
@@ -39,7 +42,7 @@ function ProjectTiles() {
                 <div className="tile">
                     <img src='https://images.unsplash.com/photo-1422393462206-207b0fbd8d6b?dpr=1&auto=format&crop=entropy&fit=crop&w=1500&h=1000&q=80' />
                     <div className="text">
-                    <h1>Lorem ipsum.</h1>
+                    {/* <h1>Lorem ipsum.</h1> */}
                     <h2 className="animate-text">More lorem ipsum bacon ipsum.</h2>
                     <p className="animate-text">Bacon ipsum dolor amet pork belly tri-tip turducken, pancetta bresaola pork chicken meatloaf. Flank sirloin strip steak prosciutto kevin turducken. </p>
                     <div className="dots">
@@ -57,7 +60,7 @@ function ProjectTiles() {
                 <div className="tile">
                     <img src='https://images.unsplash.com/photo-1422393462206-207b0fbd8d6b?dpr=1&auto=format&crop=entropy&fit=crop&w=1500&h=1000&q=80' />
                     <div className="text">
-                    <h1>Lorem ipsum.</h1>
+                    {/* <h1>Lorem ipsum.</h1> */}
                     <h2 className="animate-text">More lorem ipsum bacon ipsum.</h2>
                     <p className="animate-text">Bacon ipsum dolor amet pork belly tri-tip turducken, pancetta bresaola pork chicken meatloaf. Flank sirloin strip steak prosciutto kevin turducken. </p>
                     <div className="dots">
@@ -71,7 +74,7 @@ function ProjectTiles() {
                 <div className="tile">
                     <img src='https://images.unsplash.com/photo-1422393462206-207b0fbd8d6b?dpr=1&auto=format&crop=entropy&fit=crop&w=1500&h=1000&q=80' />
                     <div className="text">
-                    <h1>Lorem ipsum.</h1>
+                    {/* <h1>Lorem ipsum.</h1> */}
                     <h2 className="animate-text">More lorem ipsum bacon ipsum.</h2>
                     <p className="animate-text">Bacon ipsum dolor amet pork belly tri-tip turducken, pancetta bresaola pork chicken meatloaf. Flank sirloin strip steak prosciutto kevin turducken. </p>
                     <div className="dots">
@@ -85,7 +88,7 @@ function ProjectTiles() {
                 <div className="tile">
                     <img src='https://images.unsplash.com/photo-1422393462206-207b0fbd8d6b?dpr=1&auto=format&crop=entropy&fit=crop&w=1500&h=1000&q=80' />
                     <div className="text">
-                    <h1>Lorem ipsum.</h1>
+                    {/* <h1>Lorem ipsum.</h1> */}
                     <h2 className="animate-text">More lorem ipsum bacon ipsum.</h2>
                     <p className="animate-text">Bacon ipsum dolor amet pork belly tri-tip turducken, pancetta bresaola pork chicken meatloaf. Flank sirloin strip steak prosciutto kevin turducken. </p>
                     <div className="dots">
