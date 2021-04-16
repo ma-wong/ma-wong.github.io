2f3235e906e56c4e0f195466141fc8863d9dce13
Morgan <morganw118@gmail.com>
2021-04-16 16:57:53 -0700

    added screenshots for musicology and employee tracker to tiles

--- src/components/ProjectTiles/index.js
+++ src/components/ProjectTiles/index.js
@@ -1,7 +1,10 @@
 import React from "react";
 import "./style.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 import Archies from "./imgs/archies.png";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import employeeTracker from "./imgs/employee-tracker.png";
+import musicology from "./imgs/musicology.png";
 
 function ProjectTiles() {
     
@@ -10,10 +13,33 @@ function ProjectTiles() {
         <div>
 
             <div className="row wrap">
+
                 <div className="tile">
-                    <img src={Archies} />
+                    <img src={musicology} />
                     <div className="text">
                         {/* <h1>Lorem ipsum.</h1> */}
+                        <h2 className="animate-text">Musicology</h2>
+                        <p className="animate-text">
+                            A single web application that combines several APIs to allow users to easily review an music artists’ discography, social media, upcoming events, annd similar artists 
+                        </p>
+
+                        <div className="animate-text tile-icon-div">
+                            <a href="https://github.com/mfitzwinick/binary-babes" className="tile-link"><FontAwesomeIcon className="tile-icon" icon={['fab', 'github']}/></a>
+                            <a href="" className="tile-link"><FontAwesomeIcon className="tile-icon" icon="external-link-alt"/></a>
+                        </div>
+
+                        <div className="dots">
+                            <span></span>
+                            <span></span>
+                            <span></span>
+                        </div>
+                    </div>
+                </div>
+
+
+                {/* <div className="tile">
+                    <img src={Archies} />
+                    <div className="text">
                         <h2 className="animate-text">Archie's Ice Cream</h2>
                         <p className="animate-text">
                             Complete overhaul and redesign of Archie's Ice Cream website using Squarespace.
@@ -29,10 +55,10 @@ function ProjectTiles() {
                             <span></span>
                         </div>
                     </div>
-                </div>
+                </div> */}
 
                 <div className="tile">
-                    <img src='https://images.unsplash.com/photo-1458668383970-8ddd3927deed?dpr=1&auto=format&crop=entropy&fit=crop&w=1500&h=1004&q=80' />
+                    <img src={employeeTracker} />
                     <div className="text">
                         <h2 className="animate-text">Employee Tracker</h2>
                         <p className="animate-text">
@@ -43,28 +69,6 @@ function ProjectTiles() {
                             <a href="https://github.com/ma-wong/employee-tracker" className="tile-link"><FontAwesomeIcon className="tile-icon" icon={['fab', 'github']}/></a>
                             <a href="" className="tile-link"><FontAwesomeIcon className="tile-icon" icon="external-link-alt"/></a>
                         </div>
-                        <div className="dots">
-                            <span></span>
-                            <span></span>
-                            <span></span>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="tile">
-                    <img src='https://images.unsplash.com/photo-1458668383970-8ddd3927deed?dpr=1&auto=format&crop=entropy&fit=crop&w=1500&h=1004&q=80' />
-                    <div className="text">
-                        {/* <h1>Lorem ipsum.</h1> */}
-                        <h2 className="animate-text">Musicology</h2>
-                        <p className="animate-text">
-                            A single web application that combines several APIs to allow users to easily review an music artists’ discography, social media, upcoming events, annd similar artists 
-                        </p>
-
-                        <div className="animate-text tile-icon-div">
-                            <a href="https://github.com/mfitzwinick/binary-babes" className="tile-link"><FontAwesomeIcon className="tile-icon" icon={['fab', 'github']}/></a>
-                            <a href="" className="tile-link"><FontAwesomeIcon className="tile-icon" icon="external-link-alt"/></a>
-                        </div>
-
                         <div className="dots">
                             <span></span>
                             <span></span>
