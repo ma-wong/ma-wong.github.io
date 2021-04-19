aca9a682a434d9e00ad371318b22ca0ca1d4af85
Morgan <morganw118@gmail.com>
2021-04-19 15:17:22 -0700

    added new logo and got rid of placeholder

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import logo from "./img/logo.png";
 import "./style.css"
+import logo from "./img/logo.png";
 
 function Nav() {
     return (
